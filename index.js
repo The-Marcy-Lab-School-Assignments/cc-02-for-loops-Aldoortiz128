141b83f729838366e47705da9f77cd3fc87a7d05
aldoortiz128 <aldoortiz128@gmail.com>
2021-09-29 14:39:48 +0000

    All problems except bonus problem #5 solved

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 //1
 function countToTen(){
-  
+for (i = 1; i <= 10; i++) {console.log (i)}
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+for (i = 1; i <= num; i++) {console.log (i)}
 }
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(num){
+for (let i = 1; i <= num; i++){
+if (i % 2 === 0) {console.log (i);}
+  }
+
 }
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+for (let i = 1; i <= num; i++) {
+if (i %2 != 0) {console.log (i);}
 }
+}
+  
+//5
+function countEvens(arraynum){
+let arr = [];
+for (let i = 1; i <=arraynum[i]; i++) {
+if (i %2 === 0) {arr.push(i); console.log (arr);
+}
+}
+} // Still working on bonus problem #5
 
-//5 
-function countEvens(){
-  
-}
+countToTen();
+countFromOne(5);
+countEveryEven(10);
+countEveryOdd(10);
+countEvens([10, 6, 7])
 
